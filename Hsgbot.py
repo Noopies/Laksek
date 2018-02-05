8609e238844a9026a3d18870e1e264641f1acadf
Noopies <stvincen1190@gmail.com>
2018-02-05 15:43:53 +0700

    Roasted_and_Avatar

--- Hsgbot.py
+++ Hsgbot.py
@@ -8,12 +8,40 @@ client = discord.Client()
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
+    print(client.get_server)
+
+
 
 @client.event
 async def on_message(message):
-    download bokep
-    if message.content.startswith('download'):
-        if message.channel.name == "general":
-            await client.send_file(message.channel, './Image/bsg.jpg')
+    await client.change_presence(game=discord.Game(name='With Fire'))
+    if "dasar culun" in message.content :
+        if message.channel.name == "dota" and message.author== "Dest1nationX#5726" :
+            await client.send_message(message.channel,message.author.mention)
+            await client.send_file(message.channel, './Image/Roast.PNG')
+
+
+    if "dasar culun lu" in message.content :
+        await client.send_message(message.channel,message.author.mention)
+        await client.send_file(message.channel, './Image/Roast.PNG')
+
+    if message.content.startswith("!avatar"):
+        Nama= message.content.split(" ")
+
+        await client.send_message(message.channel,message.server.get_member_named(Nama[1]).avatar_url)
+
+
+
+
+    print(message.author.name)
+    
+
+
+
+
+
+
+
+
 
 client.run('NDA5MDczNjUyMDgxMDk4NzUz.DVZWVw.B_YrSej_x5fgCB4XQm3hN2BwuQI')
