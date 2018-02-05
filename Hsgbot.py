5007064b98798b802d2bd34c2e7baa6cee9ca8f4
Noopies <stvincen1190@gmail.com>
2018-02-05 16:30:28 +0700

    laksek_call

--- Hsgbot.py
+++ Hsgbot.py
@@ -8,13 +8,12 @@ client = discord.Client()
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
-    print(client.get_server)
+    await client.change_presence(game=discord.Game(name='With Fire'))
 
 
 
 @client.event
 async def on_message(message):
-    await client.change_presence(game=discord.Game(name='With Fire'))
     if "dasar culun" in message.content :
         if message.channel.name == "dota" and message.author== "Dest1nationX#5726" :
             await client.send_message(message.channel,message.author.mention)
@@ -30,11 +29,19 @@ async def on_message(message):
 
         await client.send_message(message.channel,message.server.get_member_named(Nama[1]).avatar_url)
 
+    if message.content.startswith("!assemble"):
+        if message.channel.name == "dota":
+            await client.send_message(message.channel,"@DOTA")
+            await client.send_file(message.channel, './Image/VRanger.jpg')
+            await client.send_message(message.channel,"Veiltraite Ranger Assemble")
 
+    if message.content.startswith("!salt"):
+        if message.channel.name == "gbf":
+            await client.send_file(message.channel, './Image/Salt.PNG')
 
 
     print(message.author.name)
-    
+
 
 
 
